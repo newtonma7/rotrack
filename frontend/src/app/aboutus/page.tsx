ebe323d5a141b28ebadec4ab885a348b78aaf12a
Ethan <maethan005@gmail.com>
2026-01-17 03:16:39 -0500

    draft of about us

---
+++ frontend/src/app/aboutus/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+import { colors } from "../themes";
+
+export default function SignUp() {
+
+  return (
+    <div style={{ backgroundColor: colors.primary.DEFAULT, minHeight: "100vh" }}>
+      <h1>Welcome user</h1>
+    </div>
+  );
+}
