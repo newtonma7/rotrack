7b1803acfc85f0bf8c53f3687cf16f7d007356b1
Ethan <maethan005@gmail.com>
2026-01-23 04:39:52 -0500

    Starting about us page

--- frontend/src/app/aboutus/page.tsx
+++ frontend/src/app/aboutus/page.tsx
@@ -6,8 +6,23 @@ import { colors } from "../themes";
 export default function SignUp() {
 
   return (
-    <div style={{ backgroundColor: colors.primary.DEFAULT, minHeight: "100vh" }}>
-      <h1>Welcome user</h1>
+    <div style={{ 
+      backgroundColor: colors.primary.DEFAULT, 
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontFamily: "Arial, Helvetica, sans-serif",
+      }}>
+      <div style={{marginBottom: "100px", textAlign: "center"}}>
+      <h1>About Us</h1>
+      <p> placeholder text </p>
+      </div>
+      <div style={{marginBottom: "100px", textAlign: "center"}}>
+      <h1>Our Mission</h1>
+      <p> placeholder text </p>
+      </div>
     </div>
   );
 }
